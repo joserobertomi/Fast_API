8959da797bd63e9a8b001b72c085a540e4781754
Jose Roberto Mendonca Inacio <joserobertomi@outlook.com>
2024-07-20 11:00:40 -0300

    finish project 1

--- Project1/books.py
+++ Project1/books.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Body
 
 app = FastAPI()
 
@@ -37,3 +37,22 @@ async def read_book_by_author_and_category(book_author:str, category:str):
         if book.get('author').casefold() == book_author.casefold() and book.get('category').casefold() == category.casefold():
             books_to_return.append(book)
     return books_to_return
+
+@app.post("/books/create_book")
+async def create_book(new_book=Body()):
+    BOOKS.append(new_book)
+
+
+@app.put("/books/update_book")
+async def update_book(updated_book=Body()):
+    for i in range(len(BOOKS)):
+        if BOOKS[i].get('title').casefold() == updated_book.get('title').casefold():
+            BOOKS[i] = updated_book
+
+
+@app.delete("/books/delete_book/{book_title}")
+async def delete_book(book_title: str):
+    for i in range(len(BOOKS)):
+        if BOOKS[i].get('title').casefold() == book_title.casefold():
+            BOOKS.pop(i)
+            break
